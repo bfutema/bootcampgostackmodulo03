fa2f1ff56e92608167bcf47cfea002fc5b8b34bc
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-16 16:45:58 -0300

    initial commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '71599dda345994743c9ca02691c71c5f',
+  expiresIn: '7d',
+};
---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: '9Km#47mK',
+  database: 'gobarber',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
