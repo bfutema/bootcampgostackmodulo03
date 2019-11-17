bf121dfb321d782f958708a163424b87185786bd
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-17 15:01:13 -0300

    env: Variáveis ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '6cf354be482ad3',
-    pass: 'c66b5a9699f9d6',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
