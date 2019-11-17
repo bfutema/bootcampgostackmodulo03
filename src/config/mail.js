1e1f346d34f5b14d1f194bb5e3d892d202eb293a
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-11-17 12:52:01 -0300

    config: Configurando Nodemailer

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '6cf354be482ad3',
+    pass: 'c66b5a9699f9d6',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
