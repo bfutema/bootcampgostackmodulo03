fd86a6cdd2a53f167e3a8571550ef445754628a4
Bruno Amaral Futema <bruno.futema@outlook.com>
2019-12-29 22:37:16 -0300

    Ajustes para deploy

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -28,7 +28,7 @@ class AppointmentController {
             {
               model: File,
               as: 'avatar',
-              attributes: ['id', 'url'],
+              attributes: ['id', 'path', 'url'],
             },
           ],
         },
